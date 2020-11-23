14066ddbc2c1531f3cdf2dac1fdcdf29d4af3d25
Alieno <36790612+Chryptografio@users.noreply.github.com>
2020-11-23 10:40:40 +0200

    Task

--- src/main/java/com/example/aop/practice/advice/BadCredentialsLogger.java
+++ src/main/java/com/example/aop/practice/advice/BadCredentialsLogger.java
@@ -1,30 +1,26 @@
 package com.example.aop.practice.advice;
 
 import com.example.aop.practice.model.User;
-import com.example.aop.practice.service.BadCredentialsException;
-import com.example.aop.practice.service.NonExistingUserException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 
 @Slf4j
 @Aspect
 @RequiredArgsConstructor
 public class BadCredentialsLogger {
+    
+    // TODO: make changes here
 
     private final Capturer capturer;
 
-    @AfterThrowing(pointcut = "execution(* com.example.aop.practice.service.DummyAuthenticatorService.authenticateUser(..))", throwing = "exception")
-    public void captureWrongPassword(JoinPoint joinPoint, BadCredentialsException exception) {
+    public void captureWrongPassword(JoinPoint joinPoint) {
         final User user = (User) joinPoint.getArgs()[0];
         log.info("User {} tried to access account", user.getName());
         capturer.captureWrongPassword();
     }
 
-    @AfterThrowing(pointcut = "execution(* com.example.aop.practice.service.DummyAuthenticatorService.authenticateUser(..))", throwing = "exception")
-    public void captureNonExistingUser(NonExistingUserException exception) {
+    public void captureNonExistingUser() {
         log.info("There was an attempt to login to the server");
         capturer.captureNonExistingUser();
     }
